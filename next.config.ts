5fea32a820d755c97db2b64c75e43fe62e564964
cxp <codexpoint@gmail.com>
2024-12-24 10:35:09 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,14 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   output: "export",
-  basePath: "",
-  trailingSlash: true,
-  reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "https://adsfoxy.com" : "",
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
+  // basePath: "",
+  // trailingSlash: true,
+  // reactStrictMode: true,
+  // assetPrefix: process.env.NODE_ENV === "production" ? "https://adsfoxy.com" : "",
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false, net: false, tls: false };
+  //   return config;
+  // },
   // // Add this to handle dynamic routes in static export
   // exportPathMap: async function (
   //   defaultPathMap,
